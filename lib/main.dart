959f30f2782709afaf281cbedeaac991429c267d
Hugo Montana <benzlbenny2k@gmail.com>
2022-09-24 21:34:15 +0300

    app setup

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutt/song_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SongPage(),
+    );
+  }
+}
